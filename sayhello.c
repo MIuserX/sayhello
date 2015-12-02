7dc854b18af23da24fb0380014ac9ee8746ea415
jwd <dreamerjw@yahoo.com>
2015-12-02 21:06:55 +0800

    create chPlayer function; using it in sayhello.c

--- sayhello.c
+++ sayhello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#include "tools.h"
+
 int main()
 {
     puts("hello linux git");
+
+    chPlayer('=', 10);
 
     return 0;
 }
---
+++ tools.h
@@ -0,0 +1,6 @@
+#ifndef _TOOLS_H_
+#define _TOOLS_H_
+
+void chPlayer(char ch, int times);
+
+#endif
